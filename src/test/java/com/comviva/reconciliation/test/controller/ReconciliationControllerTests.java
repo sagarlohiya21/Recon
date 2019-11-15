e95a2e62502d0220c5e271809c2b3c50453bc364
Prateek Parashar <prateek.parashar@comviva.com>
2019-11-15 19:22:01 +0530

    Add tests for controller

--- src/test/java/com/comviva/reconciliation/test/controller/ReconciliationControllerTests.java
+++ src/test/java/com/comviva/reconciliation/test/controller/ReconciliationControllerTests.java
@@ -1,7 +1,12 @@
 package com.comviva.reconciliation.test.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -13,10 +18,12 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import com.comviva.reconciliation.controller.TransactionController;
+import com.comviva.reconciliation.entity.Transaction;
 import com.comviva.reconciliation.service.TransactionService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -53,6 +60,29 @@ public class ReconciliationControllerTests {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+	}
+
+	@Test
+	public void whenValidEndPoint_sendStatusOK() throws Exception {
+		this.mockMvc.perform(get("/getAllFailedTransactions")).andExpect(status().isOk());
+	}
+
+	@Test
+	public void invalidMethods_areNotAllowed() throws Exception {
+		this.mockMvc.perform(post("/getAllFailedTransactions").contentType("application/xml").content("test"))
+				.andExpect(status().isMethodNotAllowed());
+	}
+
+	@Test
+	public void validMethodsAreAllowed() throws Exception {
+		this.mockMvc.perform(get("/getAllFailedTransactions")).andExpect(status().isOk());
+	}
+
+	@Test
+	public void expectedObject_isReturned() throws Exception {
+		MvcResult result = this.mockMvc.perform(get("/getAllFailedTransactions")).andReturn();
+
+		assertEquals(result, result);
 	}
 
 }
