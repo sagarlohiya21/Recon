f0077645a32e21b69839f6ecda8ff4519563af99
Prateek Parashar <prateek.parashar@comviva.com>
2019-11-17 12:48:27 +0530

    Removed the command line vulnerability

--- src/main/java/com/comviva/reconciliation/ReconciliationApplication.java
+++ src/main/java/com/comviva/reconciliation/ReconciliationApplication.java
@@ -10,7 +10,7 @@ import org.springframework.web.client.RestTemplate;
 public class ReconciliationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ReconciliationApplication.class, args);
-		
+		SpringApplication.run(ReconciliationApplication.class);
+
 	}
 }
